5db393786f21e9a14dc3a2ba47c1f7af5e9dcf32
Oleksii Churbanov <lyoshatop@gmail.com>
2025-04-01 18:45:12 +0300

    implement tests for py-check-password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.main import check_password
+
+
+def test_password_has_less_than_8_characters() -> None:
+    assert check_password("A5@bc") == False
+
+
+def test_password_has_more_than_16_characters() -> None:
+    assert check_password("A5@bcdefghijklmnop") == False
+
+
+def test_password_has_no_uppercase() -> None:
+    assert check_password("abc5_efgh") == False
+
+
+def test_password_has_no_digit() -> None:
+    assert check_password("Alexcyberf@") == False
+
+
+def test_password_has_no_special_symbol() -> None:
+    assert check_password("Alexcyberf5") == False
+
+
+def test_password_has_all_needed_rules() -> None:
+    assert check_password("Abcdefg5!") == True
